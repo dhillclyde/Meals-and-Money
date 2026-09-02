ea84da6e756280c4d7fd3206cd5e48cb3c9129e1
dhillclyde <dhill@clydeinc.com>
2026-09-02 16:24:45 -0600

    Create _layout.jsx

---
+++ app/_layout.jsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ title: 'Home Apps' }} />
+        <Stack.Screen name="meal" />
+        <Stack.Screen name="money" />
+      </Stack>
+    </>
+  );
+}
